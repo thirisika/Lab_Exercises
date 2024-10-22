e755ef24c5ad4bca4d5a176fdf9dcb167a2ff885
thirisika <81810634+thirisika@users.noreply.github.com>
2024-10-22 14:43:21 +1300

    Add files via upload

---
+++ Lab_3/index.js
@@ -0,0 +1,9 @@
+function changeC1(){
+    document.getElementById("heading1").innerText="Welcome to column1";
+    document.getElementById("column1").style.backgroundColor="green";
+}
+
+function changeC2(){
+    document.getElementById("heading2").innerText="Welcome to column2";
+    document.getElementById("column2").style.backgroundColor="red";
+}
